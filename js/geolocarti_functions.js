8710a9bfb99309397a347b1e4966394885754327
ramosvacca <ivan.ramos@correounivalle.edu.co>
2018-10-15 13:45:54 -0500

    error handling for smooth execution

--- js/geolocarti_functions.js
+++ js/geolocarti_functions.js
@@ -1,14 +1,17 @@
 document.getElementById("caso_estudio_link").onclick = function () {
 
-	window.history.pushState("object or string", "Title", "/GeolocartiVIS/caso_estudio.html");
+	
 	classChanger("index_link", "caso_estudio_link", "active", true);
 	document.getElementById('what_we_do_section').style.display = "none";
 	document.getElementById('extra_explanation_section').style.display = "none";
 	document.getElementById('aboutus_section').style.display = "none";
 	httpGetAsync("https://ramosvacca.github.io/GeolocartiVIS/pages/caso_estudio.txt",
 		setContentREQUEST,"mytruebody");
-	
-
+	try{
+		window.history.pushState("object or string", "Title", "/GeolocartiVIS/caso_estudio.html");
+	} catch(err) {
+		console.log(err.message)
+	}
 };
 
 function pageMaker(input_term) {
